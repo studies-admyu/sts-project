a63cf9a7d17edd0652771654ee12b135abfa7f21
studies-admyu <studies-admyu@users.noreply.github.com>
2016-06-23 18:54:45 +0300

    Fixed frame listener constructor prototype.

--- sts/src/ois/MyFrameListener.hpp
+++ sts/src/ois/MyFrameListener.hpp
@@ -11,20 +11,20 @@
 #include "scene/sts_scene_object.hpp"
 #include "MyFrameListener.hpp"
 
-class MyFrameListener : public Ogre::FrameListener {
+class MyFrameListener : public Ogre::FrameListener
+{
 private:
   OIS::InputManager* mInputManager;
   OIS::Keyboard* mKeyboard;
   OIS::Mouse* mMouse;
-  sts::SharedObject* mNode;
+  sts::SceneObject* mObject;
   Ogre::Camera *mCamera;
   float mMovementspeed;
 
 public:
-  MyFrameListener(sts::SharedObject*,Ogre::RenderWindow*);
+  MyFrameListener(sts::SceneObject*, Ogre::RenderWindow*);
   ~MyFrameListener();
   bool frameStarted (const Ogre::FrameEvent &evt);
   bool frameEnded (const Ogre::FrameEvent &evt);
   bool frameRenderingQueued (const Ogre::FrameEvent &evt);
 };
-
